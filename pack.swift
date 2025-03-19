2103feeb7cb593602287c7423105f397ef51f550
vyyvvuvvuvuvyuvvuvyuvy <157741817+vyyvvuvvuvuvyuvvuvyuvy@users.noreply.github.com>
2025-03-19 12:02:58 -0500

    Create pack.swift

---
+++ pack.swift
@@ -0,0 +1,11 @@
+/src/main/java/com/example/grannyandslendrina
+   ├── Main.java
+/assets/grannyandslendrina
+   ├── textures/
+   │   ├── items/
+   │   │   ├── granny_spawn_egg.png
+   │   │   ├── slendrina_spawn_egg.png
+   │   ├── entity/
+   │   │   ├── granny.png
+   │   │   ├── slendrina.png
+   ├── pack.mcmeta
